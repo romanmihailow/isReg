c2a691c3ba3c14923f9d83c5d1c8c991d82e728e
romanmihailow <romanmihailow@gmail.com>
2025-01-15 15:22:35 +0500

    change system.in

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,26 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        int amount = 50;
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input a number amount: ");
+        int amount = in.nextInt();
+//      int amount = 50;
         boolean isRegistered = true;
-//        int percent;
-//        if (isRegistered) {
-//            percent = 3;
-//        } else {
-//            percent = 1;
-//       }
-        int percent = isRegistered ? 3:1;
-        System.out.println(percent);
+        int percent;
+        if (isRegistered) {
+            percent = 3;
+        } else {
+            percent = 1;
+        }
+//      int percent = isRegistered ? 3:1;
+
+        int bonus = amount * percent / 100;
+
+        if (bonus > 500) {
+            bonus = 500;
+        }
+        System.out.println(bonus);
+        in.close();
     }
 
 
