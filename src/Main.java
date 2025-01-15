176efbf39f82e91bab622e3ae09fdc878ca25de1
romanmihailow <romanmihailow@gmail.com>
2025-01-15 14:48:47 +0500

    change ternarn

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,13 @@ public class Main {
     public static void main(String[] args) {
         int amount = 50;
         boolean isRegistered = true;
-        int percent;
-
-        if (isRegistered) {
-            percent = 3;
-        } else {
-            percent = 1;
-        }
+//        int percent;
+//        if (isRegistered) {
+//            percent = 3;
+//        } else {
+//            percent = 1;
+//       }
+        int percent = isRegistered ? 3:1;
         System.out.println(percent);
     }
 
